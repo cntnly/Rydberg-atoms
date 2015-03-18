c5d3b65a7c8f744fc9eed3924c5675d43677f665
cntnly <long2811@gmail.com>
2015-03-18 19:01:31 +0100

    update VdW

--- python/para.py
+++ python/para.py
@@ -2,13 +2,13 @@
 
 from __future__ import division
 # Define levels
-n1 = 60
-l1 = 1
+n1 = 50
+l1 = 49
 s1 = 1/2
 j1 = l1 + 1/2
 m1 = j1
-n2 = 60
-l2 = 1
+n2 = 51
+l2 = 49     
 s2 =1/2
 j2 = l2 + 1/2
 m2 = j2
@@ -21,7 +21,7 @@ theta = 0
 # Setup criterion
 delta_n_max = 6
 l_max = 2
-Choice = 1e7 # cut off energy for 1st order term, in Hz
+Choice = 1e5 # cut off energy for 1st order term, in Hz
 R_test = 1e-6
 Choice2 = 1* Choice
 R_test2 = 2* R_test
